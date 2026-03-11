b00d03b35bc01feb9e3fa3d131d2a48254bb6d63
jenniferyujenlin <jenniferyjlin@berkeley.edu>
2026-03-10 19:15:04 -0700

    Update runs

---
+++ runs/runs.sh
@@ -0,0 +1,5 @@
+python NT2_phase2_and_phase3.py \
+    --train_path data/splits/ClinVar.251103.missense.hg38.seq12k.BLBvsPLP_training.tsv \
+    --val_path data/splits/ClinVar.251103.missense.hg38.seq12k.BLBvsPLP_validation.tsv \
+    --output_dir results/NT2_seq12k_BLBvsPLP_lr3e-5_gradaccum \
+    --gpus 3
